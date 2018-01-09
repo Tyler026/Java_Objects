9c9115409295529e00acbc85a5a5ba32f7873ce1
Tyler Fuqua <tyler@agilityda.com>
2018-01-09 16:08:34 -0700

    Hands On Complete

---
+++ Java_Objects/com/java/object/tyler/personsFullName.java
@@ -0,0 +1,34 @@
+package com.java.object.tyler;
+
+public class personsFullName {
+	// Create a Class (FirstName, LastName, Age)
+	String firstName;
+	String lastName;
+	int age;
+	
+	// getter method
+	public String getFirstName() {
+		return firstName;
+	}
+	// setter method
+	public String setFirstName(String newFirstName) {
+		return firstName = newFirstName;
+	}
+	// getter method
+	public String getLastName() {
+		return lastName;
+	}
+	// setter method
+	public String setLastName(String newLastName) {
+		return lastName = newLastName;
+	}
+	// getter method
+	public int getAge() {
+		return age;
+	}
+	// setter method
+	public int setAge(int newAge) {
+		return age = newAge;
+	}
+	
+}	
